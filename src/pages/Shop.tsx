ae5a4d900f60b9fd221769c944462b7631bc95f7
afolabi mabunmi <brownboy3047@gmail.com>
2024-07-18 16:37:18 +0100

    chores: adjusted on the code in the shop page

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -72,39 +72,45 @@ const Shop: React.FC = () => {
         </ul>
 
         <ul
-          className="w-full md:w-[900px] h-[620px] md:h-[841px] border border-black py-[30px] px-[40px] rounded-2xl 
-        flex flex-wrap gap-10 justify-center md:justify-start sm:gap-5 overflow-y-scroll"
+          className="w-full md:w-[900px] h-[620px] md:h-[841px] border border-black py-[10px] px-[15px] md:py-[30px] md:px-[40px] rounded-2xl 
+        flex flex-wrap gap-10  md:justify-start sm:gap-5 overflow-y-scroll"
         >
           {selectedCollection &&
             selectedCollection.map((collection) => (
-              <li key={collection.id} className="w-[130px] sm:w-[250px]">
-                <img src={collection.photo} alt="suit" />
+              <li
+                key={collection.id}
+                // className="w-[130px] sm:w-[250px] flex flex-row gap-4 sm:flex-col"
+                className="w-full sm:w-[250px]"
+              >
+                <img src={collection.photo} alt="suit" className="w-full" />
 
-                <div className="flex flex-col gap-1 md:flex-row md:items-center justify-between mt-2">
-                  <p className="font-medium capitalize text-xs md:text-base">
-                    {collection.collections}
-                  </p>
-                  <p className="text-sm md:text-lg font-semibold">
-                    #{collection.price}
-                  </p>
-                </div>
+                <div>
+                  <div className="flex flex-col gap-1 md:flex-row md:items-center justify-between mt-2">
+                    <p className="font-medium capitalize text-xs md:text-base">
+                      {collection.collections}
+                    </p>
+                    <p className="text-sm md:text-lg font-semibold">
+                      #{collection.price}
+                    </p>
+                  </div>
 
-                <div className="flex flex-col gap-2 sm:flex-row sm:items-center justify-between mt-2">
-                  <button
-                    className="py-[5px] px-[8px] rounded-lg bg-black text-white text-[10px] sm:text-sm"
-                    onClick={() => handleSelectItem(collection)}
-                  >
-                    Add to cart
-                  </button>
+                  <div className="flex flex-col gap-2 sm:flex-row sm:items-center justify-between mt-2">
+                    <button
+                      className="py-[5px] px-[8px] rounded-lg bg-black text-white text-[10px] sm:text-sm"
+                      onClick={() => handleSelectItem(collection)}
+                    >
+                      Add to cart
+                    </button>
 
-                  <button
-                    className="py-[5px] px-[8px] rounded-lg bg-black bg-opacity-10 text-black 
-                  text-[10px] sm:text-sm flex items-center gap-[6px]"
-                    onClick={() => handleAddFavorite(collection)}
-                  >
-                    <span>Add to Favorite</span>
-                    <img src={heart} alt="heart" />
-                  </button>
+                    <button
+                      className="py-[5px] px-[8px] rounded-lg bg-black bg-opacity-10 text-black 
+                  text-[10px] sm:text-sm flex items-center justify-center gap-[6px]"
+                      onClick={() => handleAddFavorite(collection)}
+                    >
+                      <span className="">Add to Favorite</span>
+                      <img src={heart} alt="heart" />
+                    </button>
+                  </div>
                 </div>
               </li>
             ))}
